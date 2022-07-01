e179782f9b1b868eace2c1afd0cd39d92f887bba
tom-sugai <tom.sugai@gmail.com>
2022-07-01 12:07:08 +0900

    ArticlesTableTest implement testValidationDefault()

--- tests/TestCase/Model/Table/ArticlesTableTest.php
+++ tests/TestCase/Model/Table/ArticlesTableTest.php
@@ -70,7 +70,45 @@ class ArticlesTableTest extends TestCase
      */
     public function testValidationDefault()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        // エラーが無いとき
+        $article = $this->ArticlesTable->newEntity([
+            'title' => str_repeat('a', 10),
+            'body' => str_repeat('b', 256),
+        ]);
+        $expected = [];
+        $this->assertSame($expected, $article->getErrors());
+
+        // 必須項目が空のとき
+        $emptyArticle = $this->ArticlesTable->newEntity([
+            'title' => '',
+            'body' => '',
+        ]);
+        $expected = [
+            'title' => ['_empty' => 'This field cannot be left empty'],
+            'body' => ['_empty' => 'This field cannot be left empty'],
+        ];
+        $this->assertSame($expected, $emptyArticle->getErrors());
+
+        // 文字数が少ないとき
+        $lessArticle = $this->ArticlesTable->newEntity([
+            'title' => str_repeat('a', 9),
+            'body' => str_repeat('b', 9),
+        ]);
+        $expected = [
+            'title' => ['minLength' => 'The provided value is invalid'],
+            'body' => ['minLength' => 'The provided value is invalid'],
+        ];
+        $this->assertSame($expected, $lessArticle->getErrors());
+
+        // 文字数が多いとき
+        $moreArticle = $this->ArticlesTable->newEntity([
+            'title' => str_repeat('a', 256),
+            'body' => str_repeat('b', 256),
+        ]);
+        $expected = [
+            'title' => ['maxLength' => 'The provided value is invalid'],
+        ];
+        $this->assertSame($expected, $moreArticle->getErrors());
     }
 
     /**
@@ -82,4 +120,6 @@ class ArticlesTableTest extends TestCase
     {
         $this->markTestIncomplete('Not implemented yet.');
     }
+
+    
 }
