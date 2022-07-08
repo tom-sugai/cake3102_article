52cd28c90b9e0c14750c9b8825033ce83c51ae73
tom-sugai <tom.sugai@gmail.com>
2022-07-08 15:21:05 +0900

    add logout()

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -12,6 +12,18 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Auth->allow(['logout']);
+    }
+
+    public function logout()
+    {
+        $this->Flash->success('ログアウトしました。');
+        return $this->redirect($this->Auth->logout());
+    }
+
     public function login()
     {
         if ($this->request->is('post')) {
