e798406b710f4249f38bfa086d21c3d5f5fbc7b8
tom-sugai <tom.sugai@gmail.com>
2022-07-08 17:01:14 +0900

    add Auth->allow edit, delete

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends AppController
     public function initialize()
     {
         parent::initialize();
-        $this->Auth->allow(['logout','add']);
+        $this->Auth->allow(['logout','add','edit','delete']);
     }
 
     public function logout()
