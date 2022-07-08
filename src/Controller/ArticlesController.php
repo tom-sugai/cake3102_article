f642ea83519c59997875a45a867d40fc9eb061c3
tom-sugai <tom.sugai@gmail.com>
2022-07-08 15:56:06 +0900

    add isAuthraized()

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -144,6 +144,27 @@ class ArticlesController extends AppController
         }
     }
 
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+        // add および tags アクションは、常にログインしているユーザーに許可されます。
+        if (in_array($action, ['add', 'tags'])) {
+            return true;
+        }
+
+        // 他のすべてのアクションにはスラッグが必要です。
+        $slug = $this->request->getParam('pass.0');
+        if (!$slug) {
+            return false;
+        }
+
+        // 記事が現在のユーザーに属していることを確認します。
+        $article = $this->Articles->findBySlug($slug)->first();
+
+        return $article->user_id === $user['id'];
+    }
+
+
     /**
      * Index method
      *
