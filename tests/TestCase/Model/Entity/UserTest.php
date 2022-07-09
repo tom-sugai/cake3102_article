339b250236f779abb24fff09ed60006aab3b67ca
tom-sugai <tom.sugai@gmail.com>
2022-07-09 11:40:24 +0900

    add testSetPassword() use Cake\Auth\DefaultPasswordHasher;

--- tests/TestCase/Model/Entity/UserTest.php
+++ tests/TestCase/Model/Entity/UserTest.php
@@ -3,6 +3,7 @@ namespace App\Test\TestCase\Model\Entity;
 
 use App\Model\Entity\User;
 use Cake\TestSuite\TestCase;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * App\Model\Entity\User Test Case
@@ -47,5 +48,18 @@ class UserTest extends TestCase
     public function testInitialization()
     {
         $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    public function testSetPassword()
+    {
+        $rawPassword = 'secret';
+        $this->User->password = $rawPassword;
+        $hashedPassword = $this->User->password;
+
+        // ハッシュ化済み
+        $this->assertNotSame($rawPassword, $hashedPassword);
+
+        $hasher = new DefaultPasswordHasher();
+        $this->assertTrue($hasher->check($rawPassword, $hashedPassword));
     }
 }
