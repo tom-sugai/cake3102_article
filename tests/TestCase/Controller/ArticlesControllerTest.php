82ca059d9937da789034e41ce14ed388c23cf6e8
tom-sugai <tom.sugai@gmail.com>
2022-07-12 16:42:18 +0900

    implement edit test 3 method test記事編集ページにアクセスできる() test記事を更新し記事一覧にリダイレクトする() testバリデーションエラーだと更新できずエラーメッセージが表示される()

--- tests/TestCase/Controller/ArticlesControllerTest.php
+++ tests/TestCase/Controller/ArticlesControllerTest.php
@@ -85,6 +85,7 @@ class ArticlesControllerTest extends IntegrationTestCase
 
     public function testバリデーションエラーだと追加できずエラーメッセージが表示される()
     {
+        $this->enableCsrfToken();
         $this->session(['Auth.User.id' => 1]);
         $this->post('/articles/add', [
             'title' => 'Nintendo Switch を購入！',
@@ -92,14 +93,53 @@ class ArticlesControllerTest extends IntegrationTestCase
             'tag_string' => '',
         ]);
 
-        $this->assertResponseOk();
-        $this->assertResponseContains('Unable to add your article.');
+        //$this->assertResponseOk();
+        $this->assertResponseCode(302);
+        $this->assertResponseContains('The article could not be saved.');
 
         $this->get('/articles');
         $this->assertResponseNotContains('Nintendo Switch を購入！');
     }
 
+    public function test記事編集ページにアクセスできる()
+    {
+        $this->session(['Auth.User.id' => 1]);
+        $this->get('/articles/edit/CakePHP3-chutoriaru');
 
+        $this->assertResponseContains('記事の編集');
+        $this->assertResponseContains('CakePHP3 チュートリアル');
+    }
+
+    public function test記事を更新し記事一覧にリダイレクトする()
+    {
+        $this->session(['Auth.User.id' => 1]);
+        $this->post('/articles/edit/CakePHP3-chutoriaru', [
+            // タイトルを変更する
+            'title' => '1時間で分かるCakePHP3 チュートリアル',
+            'body' => 'このチュートリアルは簡単な CMS アプリケーションを作ります。 はじめに CakePHP のインストールを行い、データベースの作成、 そしてアプリケーションを素早く仕上げるための CakePHP が提供するツールを使います。',
+            'tag_string' => 'PHP,CakePHP',
+        ]);
+        $this->assertRedirect('/articles');
+        $this->assertSession('Your article has been updated.', 'Flash.flash.0.message');
+
+        $this->get('/articles');
+        $this->assertResponseContains('1時間で分かるCakePHP3 チュートリアル');
+    }
+
+    public function testバリデーションエラーだと更新できずエラーメッセージが表示される()
+    {
+        $this->session(['Auth.User.id' => 1]);
+        $this->post('/articles/edit/CakePHP3-chutoriaru', [
+            // タイトルを変更する
+            'title' => '1時間で分かるCakePHP3 チュートリアル',
+            'body' => '',
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseContains('Unable to update your article.');
+
+        $this->get('/articles');
+        $this->assertResponseNotContains('1時間で分かるCakePHP3 チュートリアル');
+    }
 
     /**
      * Test edit method
