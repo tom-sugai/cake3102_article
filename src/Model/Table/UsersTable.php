d5ca4932c96f9d5d852979516d021c0366288045
tom-sugai <tom.sugai@gmail.com>
2022-07-19 19:26:13 +0900

    dismiss ''

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -70,7 +70,7 @@ class UsersTable extends Table
         
         $validator
             ->add('role', 'inList', [
-                'rule' => ['inList', ['admin', 'author','']],
+                'rule' => ['inList', ['admin', 'author']],
                 'message' => 'Please enter a valid role'
             ]);
             
