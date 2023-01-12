4f52c2e457a8f3b3951b87ab901b68a713223066
tom-sugai <tom.sugai@gmail.com>
2023-01-12 12:34:03 +0900

    add use Cake\Core\Plugin;

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -42,6 +42,7 @@ use Cake\Mailer\Email;
 use Cake\Mailer\TransportFactory;
 use Cake\Utility\Inflector;
 use Cake\Utility\Security;
+use Cake\Core\Plugin;
 
 /*
  * Uncomment block of code below if you want to use `.env` file during development.
