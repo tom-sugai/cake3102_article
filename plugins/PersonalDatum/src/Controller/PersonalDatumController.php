ef9309e51b669fe7b53752758f5f1409e44410cd
tom-sugai <tom.sugai@gmail.com>
2023-01-12 12:18:34 +0900

    add controller

---
+++ plugins/PersonalDatum/src/Controller/PersonalDatumController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PersonalDatum\Controller;
+
+use App\Controller\AppController as BaseController;
+use Cake\Event\Event;
+use Cake\Network\Exception\InvalidCsrfTokenException;
+use  Cake\ORM\TableRegistory;
+
+class PersonalDatumController extends AppController
+{
+    public function index() {
+        $data = $this->PersonalDatum->find('all');
+        $this->set('data',$data);
+    }   
+}
+?>
